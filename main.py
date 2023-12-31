7efe47c21aa3dc46d2250f629be5486139b74bae
Daniel Ilies <iliesh.daniel@gmail.com>
2023-12-31 13:42:05 +0400

    logging added

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 from flask import Flask, request
+import logging
 from bot import Bot
 
 app = Flask(__name__)  # Create your Flask app instance
-bot = Bot()  # Create an instance of your Bot class
+bot = Bot(app)  # Pass the Flask app instance to your Bot class
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG)
 
 @app.route('/', methods=['POST'])
 def webhook_handler():
     if request.method == 'POST':
         update = request.get_json()
-        message = update['message']
-        bot.handle_message(message)
+
+        # Log the received update for debugging
+        logging.debug(f"Received update: {update}")
+
+        if 'message' in update:
+            message = update['message']
+
+            # Log the received message for debugging
+            logging.debug(f"Received message: {message}")
+
+            bot.handle_message(message)
+    
     return 'OK'
 
 if __name__ == "__main__":
-    bot.start() 
+    bot.start()
